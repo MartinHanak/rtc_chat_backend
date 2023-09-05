ba286d18314fa2c494fb4c2c893a75fd81b27265
Martin Hanák <martinhanak97@gmail.com>
2023-09-05 19:59:10 +0200

    Require unique name for room creation

--- src/controllers/roomSSE.ts
+++ src/controllers/roomSSE.ts
@@ -1,49 +1,47 @@
 import { Response, Router } from "express";
 import { RedisSession } from "..";
 
-
 const subscribers = new Set<Response>();
-
 
 export const roomSSERouter = Router();
 
 // SSE route for the list of available rooms
-roomSSERouter.get('/', async (req, res) => {
-    const headers = {
-      'Content-Type': 'text/event-stream',
-      'Connection': 'keep-alive',
-      'Cache-Control': 'no-cache'
-    };
+roomSSERouter.get("/", async (req, res) => {
+  const headers = {
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive",
+    "Cache-Control": "no-cache",
+  };
 
-    res.writeHead(200, headers);
+  res.writeHead(200, headers);
 
-    subscribers.add(res);
+  subscribers.add(res);
 
-    // send 1st notification
-    const rooms = await RedisSession.getAllRooms();
+  // send 1st notification
+  const rooms = await RedisSession.getAllRooms();
 
-    res.write(`data: ${JSON.stringify(rooms)}\n\n`);
+  console.log("SSE connection started");
 
-    req.on('error', () => {
-        res.status(500).json({message: `Rooms SSE controller internal server error.`})
-    });
+  res.write(`data: ${JSON.stringify(rooms)}\n\n`);
 
-    req.on('close', () => {
-        console.log('SSE connection closed');
-        res.end();
-        subscribers.delete(res);
-    })
+  req.on("error", () => {
+    console.log("Rooms SSE controller internal server error.");
+    res.end();
+    subscribers.delete(res);
+  });
 
-})
-
+  req.on("close", () => {
+    console.log("SSE connection closed");
+    res.end();
+    subscribers.delete(res);
+  });
+});
 
 // all notifications after the 1st one come from calling this function
 export async function notifySubscribers() {
+  const rooms = await RedisSession.getAllRooms();
 
-    const rooms = await RedisSession.getAllRooms();
-
-    subscribers.forEach((sub) => {
-        sub.write(`data: ${JSON.stringify(rooms)}\n\n`);
-    })
-
+  subscribers.forEach((sub) => {
+    sub.write(`data: ${JSON.stringify(rooms)}\n\n`);
+  });
 }
